f5f62f7e898ab101f716c80657f18eba6dd1d03d
Audrey Paleczny <apautumn@MacBook-Air.local>
2021-10-14 20:11:50 -0700

    Fixed rotation OOB problem :)
    OOB = out of bounds

--- BlockPuzzle/Program_Update.cs
+++ BlockPuzzle/Program_Update.cs
@@ -11,25 +11,25 @@ namespace BlockPuzzle
             // pretty switch statement :)
             switch (key.Key)
             {
-                case ConsoleKey.UpArrow: piecePosition.y--; break;
+                case ConsoleKey.UpArrow:    piecePosition.y--;      break;
 
-                case ConsoleKey.DownArrow: piecePosition.y++; break;
+                case ConsoleKey.DownArrow:  piecePosition.y++;      break;
 
-                case ConsoleKey.LeftArrow: piecePosition.x--; break;
+                case ConsoleKey.LeftArrow:  piecePosition.x--;      break;
 
-                case ConsoleKey.RightArrow: piecePosition.x++; break;
+                case ConsoleKey.RightArrow: piecePosition.x++;      break;
 
-                case ConsoleKey.Spacebar: imprintPiece(); break;
+                case ConsoleKey.Spacebar:   imprintPiece();         break;
 
-                case ConsoleKey.H: tPiece.FlipHorizontal(); break;
+                case ConsoleKey.H:          tPiece.FlipHorizontal();break;
 
-                case ConsoleKey.V: tPiece.FlipVertical(); break;
+                case ConsoleKey.V:          tPiece.FlipVertical();  break;
 
-                case ConsoleKey.B: tPiece.FlipDiagonal(); break;
+                case ConsoleKey.B:          tPiece.FlipDiagonal();  break;
 
-                case ConsoleKey.O: tPiece.RotateCCW(); break;
+                case ConsoleKey.O:          tPiece.RotateCCW();     break;
 
-                case ConsoleKey.P: tPiece.RotateCW(); break;
+                case ConsoleKey.P:          tPiece.RotateCW();      break;
 
                 default: break;
             }
@@ -37,8 +37,15 @@ namespace BlockPuzzle
             if (isPieceOOB() || isPieceCollidingWithBoard())
             {
                 //prevent rotating out of bounds (use isPieceOOB as reference)
+                //do the opposite rotation
                 piecePosition.x = oldPiecePos.x;
                 piecePosition.y = oldPiecePos.y;
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.O: tPiece.RotateCW();   break;
+                    case ConsoleKey.P: tPiece.RotateCCW();  break;
+                }
             }
 
             AABB p = new AABB
