850ebefe564f70ca8ae5a070c751625a36e72cef
NinaL129 <64241240+NinaL129@users.noreply.github.com>
2021-11-18 19:54:24 -0800

    oops player 1

--- BlockPuzzle/Program.cs
+++ BlockPuzzle/Program.cs
@@ -86,7 +86,10 @@ namespace BlockPuzzle
 
                 // this is fixing the soft drop bug i was stupid and i deleted it earlier - Nina
                 p1.idkhowthisisdifferentbutitsoksothisissoftdropplacedownthinginprogramdotcs(this);
-                p2.idkhowthisisdifferentbutitsoksothisissoftdropplacedownthinginprogramdotcs(this);
+                if (players == 2)
+                {
+                    p2.idkhowthisisdifferentbutitsoksothisissoftdropplacedownthinginprogramdotcs(this);
+                }
 
                 GetUserInput();
                 Update();
