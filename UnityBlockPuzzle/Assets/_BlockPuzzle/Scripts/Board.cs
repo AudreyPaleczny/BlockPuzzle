febc09ce92461eba70255cd0cf6b818dad27e852
hevanli <67269156+hevanli@users.noreply.github.com>
2021-11-18 21:13:05 -0800

    initialized board
    and stuff

---
+++ UnityBlockPuzzle/Assets/_BlockPuzzle/Scripts/Board.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width = 10;
+    public int height = 20;
+    public GameObject normalBlock;
+    public Vector3 blockSize = Vector3.one;
+
+    char[][] array;
+    [TextArea(10, 20)]
+    public string initial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        array = new char[height][];
+
+        for(int i = 0; i<array.Length; ++i)
+        {
+            array[i] = new char[width];
+        }
+
+        int r = 0, c = 0;
+
+        for(int i = 0; i<initial.Length; ++i)
+        {
+            if(initial[i] == '\n')
+            {
+                r++;
+                c = -1;
+            }
+            if(r < height && c < width && initial[i] != ' ' && initial[i] != '\n')
+            {
+                array[r][c] = initial[i];
+            }
+            c++;
+        }
+
+        string output = "";
+        for(int row = 0; row<height; ++row)
+        {
+            for (int col = 0; col < width; ++col)
+            {
+                if(array[row][col] != 0)
+                {
+                    output += array[row][col];
+                }
+                else
+                {
+                    output += '.';
+                }
+            }
+            output += '\n';
+        }
+        Debug.Log(output);
+
+        for (int col = 0; col<width; col++)
+        {
+            for (int row = 0; row < height; row++)
+            {
+                if(array[row][col] != 0)
+                {
+                    GameObject go = Instantiate(normalBlock);
+                    go.transform.SetParent(transform);
+                    go.transform.localPosition = new Vector3(col * blockSize.x, row * blockSize.y, 0);
+                }
+                
+            }
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
