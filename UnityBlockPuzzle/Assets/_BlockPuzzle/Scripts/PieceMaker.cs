f3e7189ca166987b348fecb264636698c84cf6de
NinaL129 <64241240+NinaL129@users.noreply.github.com>
2022-01-11 21:33:19 -0800

    7bag! woohoo!

--- UnityBlockPuzzle/Assets/_BlockPuzzle/Scripts/PieceMaker.cs
+++ UnityBlockPuzzle/Assets/_BlockPuzzle/Scripts/PieceMaker.cs
@@ -34,11 +34,72 @@ public class PieceMaker : MonoBehaviour
         listOfObjects.RemoveAt(last);
     }
 
+    // 7 bag implementation here
+    // the pieces are not random everytime we launch the game because the seed is the same
+
+    public int[] nums;
+    public int numsLeft;
+    // public double p = Random.Range(0f, 1f);
+
+    public void initNums(int n)
+    {
+        nums = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = i;
+
+            numsLeft = n;
+        }
+
+        shuffle(nums);
+    }
+
+    public void shuffle(int[] a)
+    {
+        for (int i = a.Length-1; i > 0; i--)
+        {
+            int j = UnityEngine.Random.Range(0, i);
+            int temp = a[j];
+            a[j] = a[i];
+            a[i] = temp;
+        }
+    }
+
+    public int nextInt()
+    {
+        // If there are no numbers left, just return 0
+        if (numsLeft == 0) return 7;
+
+        // Get a random index amongst the remaining numbers
+        // int index = (int) (numsLeft * p);
+
+        // The number of numbers left is decreased and numsLeft now
+        // references the last index in the array in which a unique value
+        // can be returned
+        numsLeft--;
+
+        // Save the return value
+        //int temp = nums[index];
+
+        // Overwrite the element in position index with the
+        // element in the last position containing a number
+        // that has yet to be returned
+        ///nums[index] = nums[numsLeft];
+
+        // Return the selected value
+        return nums[numsLeft];
+    }
+
     public void makeAnotherOne()
     {
-        int p = UnityEngine.Random.Range(0, prefabsOfPieces.Count);
+        int rgn = nextInt();
+        if (rgn == 7)
+        {
+            initNums(7);
+            rgn = nextInt();
+        }
 
-        GameObject newOne = Instantiate(prefabsOfPieces[p]);
+        GameObject newOne = Instantiate(prefabsOfPieces[rgn]);
         newOne.transform.position = transform.position;
         newOne.transform.SetParent(transform);
         listOfObjects.Add(newOne);
@@ -96,6 +157,8 @@ public class PieceMaker : MonoBehaviour
     void Start()
     {
         debugText = GameObject.Find("Debug Text")?.GetComponent<Text>();
+        //Random.InitState(System.Environment.TickCount);
+        initNums(7);
         makeAnotherOne();
     }
 
