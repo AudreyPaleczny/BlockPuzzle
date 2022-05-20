5409e83ed657214fac193b65b5db8534221efe5f
Audrey Paleczny <apautumn@MacBook-Air.local>
2022-05-19 20:06:27 -0700

    Back to Menu Button

---
+++ UnityBlockPuzzle/Assets/_BlockPuzzle/Scripts/BacktoMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BacktoMenu : MonoBehaviour
+{
+    public void back()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+}
